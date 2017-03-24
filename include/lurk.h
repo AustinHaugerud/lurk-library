9128ca6285442e41f746e317bc0188fad390a284
AustinHaugerud <austin15328@hotmail.com>
2017-03-23 19:35:36 -0700

    Implement lurk.h header so it actually includes the whole library

--- include/lurk.h
+++ include/lurk.h
@@ -5,4 +5,14 @@
 #ifndef LURK_LURK_H
 #define LURK_LURK_H
 
+#include"messages/lurk_messages.h"
+
+#include"lurk_client_connection.h"
+#include"lurk_data_source.h"
+#include"lurk_listener.h"
+#include"lurk_message_allocator.h"
+#include"lurk_protocol_message.h"
+#include"lurk_recipient.h"
+#include"lurk_server_connection.h"
+
 #endif //LURK_LURK_H
