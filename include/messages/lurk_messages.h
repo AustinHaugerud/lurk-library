38d61a3d6b2b99b7d2343a373315f05f8acf98e3
AustinHaugerud <austin15328@hotmail.com>
2017-03-23 19:33:13 -0700

    Added lurk_messages.h header that includes all LURK protocol message types

---
+++ include/messages/lurk_messages.h
@@ -0,0 +1,23 @@
+//
+// Created by reaver on 3/23/17.
+//
+
+#ifndef LURK_LURK_MESSAGES_H
+#define LURK_LURK_MESSAGES_H
+
+#include"lurk_accept.h"
+#include"lurk_changeroom.h"
+#include"lurk_character.h"
+#include"lurk_connection.h"
+#include"lurk_error.h"
+#include"lurk_fight.h"
+#include"lurk_game.h"
+#include"lurk_leave.h"
+#include"lurk_loot.h"
+#include"lurk_message.h"
+#include"lurk_pvpfight.h"
+#include"lurk_pvpfight.h"
+#include"lurk_room.h"
+#include"lurk_start.h"
+
+#endif //LURK_LURK_MESSAGES_H
