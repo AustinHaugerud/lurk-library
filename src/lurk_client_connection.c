99cfa1bdc6c9f10c1a541fdc69a9feeb8f3543a4
AustinHaugerud <austin15328@hotmail.com>
2017-04-18 14:08:40 -0700

    Implemented basic lurk_client_connection functions.

--- src/lurk_client_connection.c
+++ src/lurk_client_connection.c
@@ -2,6 +2,21 @@
 // Created by reaver on 3/18/17.
 //
 
-#include"lurk_listener.h"
+#include"lurk_client_connection.h"
+#include<stdlib.h>
+#include<unistd.h>
 
+struct lurk_client_connection * lurk_client_connection_allocate()
+{
+    return malloc(sizeof(struct lurk_client_connection));
+}
 
+void lurk_client_connection_free(struct lurk_client_connection * con)
+{
+    free(con);
+}
+
+void lurk_client_connection_close(struct lurk_client_connection * con)
+{
+    close(con->socket);
+}
