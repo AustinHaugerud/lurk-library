bafec7990c59639b9880c175aa4c15b08899dd0f
AustinHaugerud <austin15328@hotmail.com>
2017-04-30 14:13:00 -0700

    Added C++ header

---
+++ include/lurk.hpp
@@ -0,0 +1,18 @@
+//
+// Created by reaver on 4/30/17.
+//
+
+#ifndef LURK_LURK_HPP
+#define LURK_LURK_HPP
+
+#ifdef __cplusplus
+
+extern "C"
+{
+    #include"lurk.h"
+    #include"messages/lurk_messages.h"
+};
+
+#endif
+
+#endif //LURK_LURK_HPP
